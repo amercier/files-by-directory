8cbb8171662db1c7c5772f2cdd17b2096dcd4bfd
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-11-22 11:36:38 +0100

    Added paths cleanup in filesByDirectory()

--- src/files-by-directory.js
+++ src/files-by-directory.js
@@ -1,6 +1,8 @@
 import regeneratorRuntime from 'regenerator-runtime';
 import { asyncMap, asyncFlattenMap, values } from './async';
 import File from './file';
+import { isDescendant } from './path';
+
 /**
  * Generate array of file paths for each directory, recursively.
  *
@@ -14,15 +16,18 @@ import File from './file';
  * @yields {Promise<File[]>} Generates one array of File instances per directory.
  */
 export default async function* filesByDirectory(paths) {
-  const unordered = await values(File.fromPaths(paths));
+  // Remove doubles and descendant paths
+  const cleanPaths = paths.filter(
+    (path, i) => !paths.some((other, j) => (j > i && path === other) || isDescendant(path, other)),
+  );
+
+  const unordered = await values(File.fromPaths(cleanPaths));
   const ordered = [
     ...unordered.filter(file => file.isDirectory),
     ...unordered.filter(file => !file.isDirectory),
   ];
-  const processedPaths = [];
 
-  yield* asyncMap(
-    asyncFlattenMap(ordered, file => file.getFilesByDirectory(processedPaths)),
-    files => files.map(file => file.path),
+  yield* asyncMap(asyncFlattenMap(ordered, file => file.getFilesByDirectory()), files =>
+    files.map(file => file.path),
   );
 }
